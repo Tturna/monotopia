edda7cea952a9362beb56e31590037bfd1b9c11c
Tturna <42873350+Tturna@users.noreply.github.com>
2026-06-17 18:33:39 +0300

    clean up logs

--- scripts/BaseUnit.cs
+++ scripts/BaseUnit.cs
@@ -83,7 +83,6 @@ public abstract partial class BaseUnit : Sprite2D
     {
         if (!Multiplayer.IsServer())
         {
-            DebugUtility.Print("Client requested to move unit. Calling RPC...");
             RpcId(1, MethodName.RequestMoveToTile, tilePosition);
             return;
         }
@@ -93,13 +92,11 @@ public abstract partial class BaseUnit : Sprite2D
         // If server called this RPC or if server called this directly without RPC
         if (callerId == 1 || callerId == 0)
         {
-            DebugUtility.Print("Server is moving a unit for itself");
             TryMoveToTile(tilePosition);
         }
         // If a client called this
         else
         {
-            DebugUtility.Print("Server is moving a unit on behalf of a client");
             // Ensure its their turn and stuff
             TryMoveToTile(tilePosition);
         }
@@ -118,11 +115,7 @@ public abstract partial class BaseUnit : Sprite2D
         // and does the unit have enough movement for it?
 
         // TODO: Make it so you can swap friendly units?
-        if (EntitySelector.TryGetUnit(tilePosition, out var unit) && unit is not null)
-        {
-            DebugUtility.Print($"no unit there bro ({tilePosition})");
-            return false;
-        }
+        if (EntitySelector.TryGetUnit(tilePosition, out var unit) && unit is not null) return false;
 
         Rpc(MethodName.SetUnitTilePosition, tilePosition);
 
@@ -130,9 +123,7 @@ public abstract partial class BaseUnit : Sprite2D
         {
             if (city.OwnerEmpire != OwnerEmpire)
             {
-                DebugUtility.Print($"Calling city release RPC function on empire {city.OwnerEmpire.EmpireUid}");
                 city.OwnerEmpire.RequestReleaseCity(city.CityUid);
-                DebugUtility.Print($"Calling city annex RPC function on empire {OwnerEmpire.EmpireUid}");
                 OwnerEmpire.RequestAnnexCity(city.CityUid);
             }
         }
