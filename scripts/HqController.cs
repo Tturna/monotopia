4a988705e15355062d6406ac991a94de40c2783e
Tturna <42873350+Tturna@users.noreply.github.com>
2026-08-02 14:08:30 +0300

    cleanup hq controller

--- scripts/HqController.cs
+++ scripts/HqController.cs
@@ -3,26 +3,15 @@ using Godot;
 #nullable enable
 public partial class HqController : TileController
 {
-    [Export]
-    public bool Freeze;
-
     public string HqUid { get; private set; } = null!;
     public int CoinsGenerated { get; private set; } = 2;
     public Vector2I HqTilePosition { get; private set; }
     public EmpireController OwnerEmpire { get; private set; } = null!;
 
-    private TurnSystem turnSystem = null!;
-
-    public override void _Ready()
-    {
-        turnSystem = GodotUtilities.FindNodeOfType<TurnSystem>(GetTree().Root);
-    }
-
     public void InitializeHq(Vector2I tilePosition, EmpireController ownerEmpire, string newHqUid)
     {
         HqUid = newHqUid;
         OwnerEmpire = ownerEmpire;
-
         HqTilePosition = tilePosition;
     }
 
